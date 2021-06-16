e3e93ab1df54f28124e617a0dca24e2c56deb5d3
Carter Smith <67392536+cartersmithcraney@users.noreply.github.com>
2021-06-16 20:59:03 +0000

    Updated Code

--- snake.js
+++ snake.js
@@ -1,11 +1,16 @@
 class Snake {
 
   constructor() {
-
+this.body = []; 
+this.body[0] = createVector(floor(scaleWidth / 2), floor(scaledHeight / 2));
   }
 
   update() {
-
+let head = this.body[this.body.length - 1].copy(); 
+this.body.shift(); 
+head.x += this.xDirection; 
+head.y += this.yDirection; 
+this.body.push(head);
   }
 
   endGame() {
@@ -21,11 +26,16 @@ class Snake {
   }
 
   show() {
-
+for(let i = 0; i < this.body.length; i++) { 
+  fill(0); 
+  noStroke(); 
+  rect(this.body[i].x, this.body[i].y, 1, 1);
+}
   }
 
   setDirection(x, y) {
-
+ this.xDirection = x; 
+ this.xDirection = y;
   }
 
 }
